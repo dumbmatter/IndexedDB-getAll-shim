5ea32e30a49689bd5de515a88636898cbb2f6710
Jeremy Scheff <jdscheff@gmail.com>
2012-09-03 16:45:18 -0400

    Indent with spaces, not tabs.

--- IndexedDB-getAll-shim.js
+++ IndexedDB-getAll-shim.js
@@ -1,91 +1,91 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	var Event, IDBIndex, IDBObjectStore, IDBRequest;
+    var Event, IDBIndex, IDBObjectStore, IDBRequest;
 
-	IDBObjectStore = window.IDBObjectStore || window.webkitIDBObjectStore || window.mozIDBObjectStore || window.msIDBObjectStore;
-	IDBIndex = window.IDBIndex || window.webkitIDBIndex || window.mozIDBIndex || window.msIDBIndex;
+    IDBObjectStore = window.IDBObjectStore || window.webkitIDBObjectStore || window.mozIDBObjectStore || window.msIDBObjectStore;
+    IDBIndex = window.IDBIndex || window.webkitIDBIndex || window.mozIDBIndex || window.msIDBIndex;
 
-	// https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/IDBRequest.js
-	IDBRequest = function () {
-	    this.onsuccess = null;
-	    this.readyState = "pending";
-	};
-	// https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/Event.js
-	Event = function (type, debug) {
-		return {
-			"type": type,
-			debug: debug,
-			bubbles: false,
-			cancelable: false,
-			eventPhase: 0,
-			timeStamp: new Date()
-		};
-	};
+    // https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/IDBRequest.js
+    IDBRequest = function () {
+        this.onsuccess = null;
+        this.readyState = "pending";
+    };
+    // https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/Event.js
+    Event = function (type, debug) {
+        return {
+            "type": type,
+            debug: debug,
+            bubbles: false,
+            cancelable: false,
+            eventPhase: 0,
+            timeStamp: new Date()
+        };
+    };
 
-	if (typeof IDBObjectStore.prototype.getAll === "undefined") {
-		IDBObjectStore.prototype.getAll = function (key) {
-			var objectStore, request, result;
+    if (typeof IDBObjectStore.prototype.getAll === "undefined") {
+        IDBObjectStore.prototype.getAll = function (key) {
+            var objectStore, request, result;
 
-			key = typeof key !== "undefined" ? key : null;
+            key = typeof key !== "undefined" ? key : null;
 
-			request = new IDBRequest();
-			objectStore = this;
-			result = [];
+            request = new IDBRequest();
+            objectStore = this;
+            result = [];
 
-			objectStore.openCursor(key).onsuccess = function (event) {
-				var cursor, e, target;
+            objectStore.openCursor(key).onsuccess = function (event) {
+                var cursor, e, target;
 
-				cursor = event.target.result;
-				if (cursor) {
-					result.push(cursor.value);
-					cursor.continue();
-				} else {
-					if (typeof request.onsuccess === "function") {
-						e = new Event("success");
-						e.target = {
-							readyState: "done",
-							result: result
-						};
-						request.onsuccess(e);
-					}
-				}
-			};
+                cursor = event.target.result;
+                if (cursor) {
+                    result.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    if (typeof request.onsuccess === "function") {
+                        e = new Event("success");
+                        e.target = {
+                            readyState: "done",
+                            result: result
+                        };
+                        request.onsuccess(e);
+                    }
+                }
+            };
 
-			return request;
-		};
-	}
+            return request;
+        };
+    }
 
-	if (typeof IDBIndex.prototype.getAll === "undefined") {
-		IDBIndex.prototype.getAll = function (key) {
-			var index, request, result;
+    if (typeof IDBIndex.prototype.getAll === "undefined") {
+        IDBIndex.prototype.getAll = function (key) {
+            var index, request, result;
 
-			key = typeof key !== "undefined" ? key : null;
+            key = typeof key !== "undefined" ? key : null;
 
-			request = new IDBRequest();
-			index = this;
-			result = [];
+            request = new IDBRequest();
+            index = this;
+            result = [];
 
-			index.openCursor(key).onsuccess = function (event) {
-				var cursor, e, target;
+            index.openCursor(key).onsuccess = function (event) {
+                var cursor, e, target;
 
-				cursor = event.target.result;
-				if (cursor) {
-					result.push(cursor.value);
-					cursor.continue();
-				} else {
-					if (typeof request.onsuccess === "function") {
-						e = new Event("success");
-						e.target = {
-							readyState: "done",
-							result: result
-						};
-						request.onsuccess(e);
-					}
-				}
-			};
+                cursor = event.target.result;
+                if (cursor) {
+                    result.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    if (typeof request.onsuccess === "function") {
+                        e = new Event("success");
+                        e.target = {
+                            readyState: "done",
+                            result: result
+                        };
+                        request.onsuccess(e);
+                    }
+                }
+            };
 
-			return request;
-		};
-	}
+            return request;
+        };
+    }
 }());
