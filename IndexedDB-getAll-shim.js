3943130d3214a375dd062398a02352d614b643c0
Jeremy Scheff <jdscheff@gmail.com>
2016-07-24 14:25:11 -0400

    Slightly more correct objects and error handling

--- IndexedDB-getAll-shim.js
+++ IndexedDB-getAll-shim.js
@@ -16,21 +16,47 @@
         return;
     }
 
-    // https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/IDBRequest.js
+    // IDBRequest and Event objects mostly from https://github.com/dumbmatter/fakeIndexedDB
     IDBRequest = function () {
+        this.result = null;
+        this.error = null;
+        this.source = null;
+        this.transaction = null;
+        this.readyState = 'pending';
         this.onsuccess = null;
-        this.readyState = "pending";
-    };
-    // https://github.com/axemclion/IndexedDBShim/blob/gh-pages/src/Event.js
-    Event = function (type, debug) {
-        return {
-            "type": type,
-            debug: debug,
-            bubbles: false,
-            cancelable: false,
-            eventPhase: 0,
-            timeStamp: new Date()
+        this.onerror = null;
+
+        this.toString = function () {
+            return '[object IDBRequest]';
         };
+    };
+    Event = function (type) {
+        this.type = type;
+        this.target = null;
+        this.currentTarget = null;
+
+        this.NONE = 0;
+        this.CAPTURING_PHASE = 1;
+        this.AT_TARGET = 2;
+        this.BUBBLING_PHASE = 3;
+        this.eventPhase = this.NONE;
+
+        this.stopPropagation = function () {
+            console.log('stopPropagation not implemented in IndexedDB-getAll-shim');
+        };
+        this.stopImmediatePropagation = function () {
+            console.log('stopImmediatePropagation not implemented in IndexedDB-getAll-shim');
+        };
+
+        this.bubbles = false;
+        this.cancelable = false;
+        this.preventDefault = function () {
+            console.log('preventDefault not implemented in IndexedDB-getAll-shim');
+        };
+        this.defaultPrevented = false;
+
+        this.isTrusted = false;
+        this.timestamp = Date.now();
     };
 
     // Based on spec draft https://w3c.github.io/IndexedDB/#dom-idbobjectstore-getall
@@ -43,7 +69,9 @@
         result = [];
 
         // this is either an IDBObjectStore or an IDBIndex, depending on the context.
-        this.openCursor(key).onsuccess = function (event) {
+        var cursorRequest = this.openCursor(key);
+
+        cursorRequest.onsuccess = function (event) {
             var cursor, e;
 
             cursor = event.target.result;
@@ -64,6 +92,11 @@
                 request.result = result;
                 request.onsuccess(e);
             }
+        };
+
+        cursorRequest.onerror = function (event) {
+            console.log('IndexedDB-getAll-shim error when getting data:', event.target.error);
+            request.onerror(event);
         };
 
         return request;
